220436465482cec8e9d514cd2b1e132ca7d5d0ad
JAHowe <joelandhowe@outlook.com>
2016-12-01 14:25:59 +0000

    Project
    Test

---
+++ ApplicationForCITesting/ApplicationForCITesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApplicationForCITesting.Startup))]
+namespace ApplicationForCITesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
